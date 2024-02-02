0c66461f7bcee18c3465363c39d14f3e7b53e576
KevinCyndaquil <aleplantsvsz@gmail.com>
2024-02-02 11:39:33 -0600

    first commit

---
+++ src/test/java/org/puig/puigapi/PuigApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.puig.puigapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuigApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
